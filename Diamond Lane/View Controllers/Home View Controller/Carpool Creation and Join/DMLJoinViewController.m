8c433f1c5b43f296c7ad78a0d2aa028e808bed7b
Kieran Cairney <kierancairney@hotmail.com>
2015-09-19 18:57:42 -0400

    - alter shifting multiple views

--- Diamond Lane/View Controllers/Home View Controller/Carpool Creation and Join/DMLJoinViewController.m
+++ Diamond Lane/View Controllers/Home View Controller/Carpool Creation and Join/DMLJoinViewController.m
@@ -11,6 +11,9 @@
 @interface DMLJoinViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *codeTextField;
+@property (weak, nonatomic) IBOutlet UIView* shiftView;
+
+@property (nonatomic) BOOL keyboardIsVisible;
 
 @end
 
@@ -29,6 +32,22 @@
     
 }
 
+- (void)viewDidLoad {
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardBegan:) name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardEnded:) name:UIKeyboardWillHideNotification object:nil];
+    
+    self.keyboardIsVisible = NO;
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    
+    [self.view endEditing:YES];
+    
+}
+
 - (IBAction)continueButtonPressed {
     
     [self dismissViewControllerAnimated:YES completion:nil];
@@ -38,6 +57,47 @@
 - (void)cancelButtonPressed {
     
     [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+- (void)keyboardBegan:(NSNotification *)notification
+{
+    if(!self.keyboardIsVisible) {
+        
+        NSDictionary* keyboardInfo = [notification userInfo];
+        NSValue* keyboardFrameBegin = [keyboardInfo valueForKey:UIKeyboardFrameBeginUserInfoKey];
+        CGRect keyboardFrameBeginRect = [keyboardFrameBegin CGRectValue];
+        
+        [UIView beginAnimations:nil context:NULL];
+        [UIView setAnimationDuration:[[notification userInfo][UIKeyboardAnimationDurationUserInfoKey] floatValue]];
+        [UIView setAnimationCurve:[[notification userInfo][UIKeyboardAnimationCurveUserInfoKey] integerValue]];
+        [UIView setAnimationBeginsFromCurrentState:YES];
+        [UIView commitAnimations];
+        
+        float shiftConstant = ((keyboardFrameBeginRect.size.height) / 2) - self.navigationController.navigationBar.frame.size.height;
+        
+        self.shiftView.transform = CGAffineTransformMakeTranslation(0, -shiftConstant);
+        
+        self.keyboardIsVisible = YES;
+        
+    }
+}
+
+- (void)keyboardEnded:(NSNotification *)notification {
+    
+    if(self.keyboardIsVisible) {
+        
+        [UIView beginAnimations:nil context:NULL];
+        [UIView setAnimationDuration:[[notification userInfo][UIKeyboardAnimationDurationUserInfoKey] floatValue]];
+        [UIView setAnimationCurve:[[notification userInfo][UIKeyboardAnimationCurveUserInfoKey] integerValue]];
+        [UIView setAnimationBeginsFromCurrentState:YES];
+        [UIView commitAnimations];
+        
+        self.shiftView.transform = CGAffineTransformIdentity;
+        
+        self.keyboardIsVisible = NO;
+        
+    }
     
 }
 
